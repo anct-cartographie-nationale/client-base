1ed6a8d45ff25a4cdc7dd4d9d724f01ba2770672
Marc Gavanier <marc.gavanier@gmail.com>
2022-05-04 18:54:39 +0200

    add configuraions form res'in project

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '@angular-common/(.*)': '<rootDir>/projects/client-application/src/utils/angular-common/$1',
     '@gouvfr-anct/(.*)': '<rootDir>/dist/@gouvfr-anct/$1'
   }
 };
