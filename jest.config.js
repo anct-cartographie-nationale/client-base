6520484e2e00735682990691922654de632c8bdc
Marc Gavanier <marc.gavanier@gmail.com>
2022-05-31 11:25:38 +0200

    fix: remove jest configuration from base project

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '@gouvfr-anct/(.*)': '<rootDir>/dist/@gouvfr-anct/$1'
-  }
-};
